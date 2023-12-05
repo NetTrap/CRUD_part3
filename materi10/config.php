f031abe8301108562a88215dcd2a9c0ede39e2fc
NetTrap <zaqinub@gmail.com>
2023-12-05 21:15:57 +0700

    Part 3

---
+++ materi10/config.php
@@ -0,0 +1,9 @@
+<?php
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "basisdata";
+$conn = mysqli_connect ($server, $user, $pass, $database);
+if (!$conn) {
+    die("Koneksi ke databse gagal: " . mysqli_connect_error());}
+?>
